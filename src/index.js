3518ca809d719e235a50ab82d18f4c7fbe00d1df
JorSchmitz <jorsch.pl@gmail.com>
2023-04-28 21:02:10 -0500

    feat: basic configs, connection to database

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const db = require('./database/database')
+
+const config = require('./database/config/config').api
+
+const app = express()
+
+app.use(express.json())
+
+db.authenticate()
+  .then(() => {
+    console.log('Database authenticated')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+db.sync()
+  .then(() => {
+    console.log('Database synced')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+app.get('/', ({ res }) => {
+  return res.json({
+    status: 'Up',
+    maintenance: false,
+  })
+})
+
+if (config.nodeEnv != 'test') {
+  app.listen(config.port, () => {
+    console.log(`Server started on port: ${config.port}`)
+  })
+}
